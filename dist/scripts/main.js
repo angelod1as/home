0dc41b96ab7ad1efe14558bbcd470eb751ef9690
angelod1as <angelo@angelodias.com.br>
2017-12-16 10:34:15 -0200

    still testing

--- dist/scripts/main.js
+++
@@ -1,1 +0,0 @@
-var isMobile=function(){return!!(navigator.userAgent.match(/Android/i)||navigator.userAgent.match(/webOS/i)||navigator.userAgent.match(/iPhone/i)||navigator.userAgent.match(/iPad/i)||navigator.userAgent.match(/iPod/i)||navigator.userAgent.match(/BlackBerry/i)||navigator.userAgent.match(/Windows Phone/i))};isMobile();
